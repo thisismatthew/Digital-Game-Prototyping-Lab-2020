bb7a8657434ce83c7956684e5865e331b4f31127
Lachlan McCaskill <101627963@student.swin.edu.au>
2020-12-23 17:47:52 +1100

    NEW: Added a script that moves an agent on a tilemap (does not snap to grid yet)

---
+++ Hooch Gang/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CharacterMovement : MonoBehaviour
+{  
+    public Tilemap map;
+    private Vector3 destination;
+
+    [SerializeField] private float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        destination = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            MouseClick();
+        }
+        if(Vector3.Distance(transform.position, destination) > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, movementSpeed * Time.deltaTime);
+        }
+    }
+
+    private void MouseClick()
+    {
+        Vector2 mousePosition = Input.mousePosition; //Vector 2 to Vector 3?
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector3Int gridPosition = map.WorldToCell(mousePosition);
+        if(map.HasTile(gridPosition))
+        {
+            destination = mousePosition;
+        }
+    }
+}
