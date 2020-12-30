6ad1ab1408f36eaf8247365b2f6b18f26dcda3ee
Kieran Freame <102109190@student.swin.edu.au>
2020-12-31 10:57:44 +1100

    New Hooch Gang Project in Unity 3D

---
+++ HoochGang3D/Assets/Scripts/Node.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private Renderer rend;
+    private Color startColor;
+
+    [SerializeField] private GameManager gm;
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseDown()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        gm.currentCharacter.GetComponent<Movement>().goal = this.gameObject.transform;
+    }
+
+    void OnMouseEnter()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+        
+        
+        rend.material.color = Color.green;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
