5cf8cf0ed6f149b020e0eae67a8c77d653cf14e6
Kieran Freame <102109190@student.swin.edu.au>
2021-01-07 11:23:35 +1100

    Update LineOfSight.cs
    Added simulation of attacking the enemy

--- HoochGang3D/Assets/Scripts/LineOfSight.cs
+++ HoochGang3D/Assets/Scripts/LineOfSight.cs
@@ -15,11 +15,18 @@ public class LineOfSight : MonoBehaviour
     {
         Ray ray = new Ray(transform.position, transform.forward);
 
+        Debug.DrawRay(transform.position, transform.forward);
+
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             if (hit.collider.gameObject.CompareTag("Enemy"))
             {
                 this.gameObject.GetComponent<Movement>().SetDest(hit.collider.gameObject.GetComponent<Movement>().agent.destination);
+                if (Vector3.Distance(transform.position, gameObject.GetComponent<Movement>().agent.destination) < 3f)
+                {
+                    Destroy(hit.collider.gameObject);
+                    return;
+                }
             }
         }
     }
