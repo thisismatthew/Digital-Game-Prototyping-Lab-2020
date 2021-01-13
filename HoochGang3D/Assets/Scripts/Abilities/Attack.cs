0eb3fcd9ba44ffd96adce598e1f247641dd8f8f0
Lachlan McCaskill <101627963@student.swin.edu.au>
2021-01-13 17:11:31 +1100

    NEW: Started Attack script

---
+++ HoochGang3D/Assets/Scripts/Abilities/Attack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : Ability
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
