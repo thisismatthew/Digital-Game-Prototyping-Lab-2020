2c24bacc6e01bb174263bbe4a8b7604bff883942
Lachlan McCaskill <101627963@student.swin.edu.au>
2021-01-14 14:13:56 +1100

    NEW: Added a script to detail how projectiles behave

---
+++ HoochGang3D/Assets/Scripts/Projectile.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Transform target;
+    public float speed = 70f;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+    void Update()
+    {
+        if(target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
+    }
+
+    void HitTarget()
+    {
+        Damage(target);
+        Destroy(gameObject);
+    }
+    
+    void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+}
