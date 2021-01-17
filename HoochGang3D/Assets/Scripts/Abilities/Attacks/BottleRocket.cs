8e7e52224598b7fdab8cde72100082b26dc6c2d6
Kieran Freame <102109190@student.swin.edu.au>
2021-01-17 15:40:50 +1100

    Revert "Revert "Merge branch 'kieran' of https://github.com/thisismatthew/Digital-Game-Prototyping-Lab-2020 into kieran""
    This reverts commit 2285a8179806745448438a026d60d688b4dac972.

---
+++ HoochGang3D/Assets/Scripts/Abilities/Attacks/BottleRocket.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+<<<<<<< HEAD:HoochGang3D/Assets/Scripts/Abilities/Attacks/Attack.cs
+public abstract class Attack : Ability
+{
+    public GameObject projectile;
+    private GameObject target;
+    private LineOfSight lineOfSight;
+
+    protected override void Start()
+    {
+        //get the line of sight component
+        lineOfSight = gameObject.GetComponent<LineOfSight>();
+=======
+public class BottleRocket : Attack
+{
+    public override void DisplayRange()
+    {
+        //TODO: Display ranged once the range has been determined
+        //Debug.Log("Displaying Range");
+>>>>>>> d6fde0bf84c6543f22d30ed76d5813157f4bf703:HoochGang3D/Assets/Scripts/Abilities/Attacks/BottleRocket.cs
+    }
+
+    public override void Execute()
+    {
+        foreach(GameObject g in lineOfSight.GetTargetsInRange()) //find all targets within line of sight
+        {
+            //check if target is within range
+            
+            //instantiate projectile
+            GameObject firedProjectile = Instantiate(projectile, transform.position, Quaternion.identity);
+            Projectile projectileScript = firedProjectile.GetComponent<Projectile>();
+            projectileScript.Seek(g.transform);
+        }
+    }
+
+    public GameObject Target
+    {
+        get { return target; }
+        set
+        {
+            if (value.CompareTag(GetComponent<LineOfSight>().enemyTag))
+            {
+                target = value;
+            }   
+        }
+    }
+}
