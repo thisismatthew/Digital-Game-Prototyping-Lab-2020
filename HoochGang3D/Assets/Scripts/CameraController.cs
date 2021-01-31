2acd2fe17ff999b4e1a5f7ab84b696961a6c9243
Lachlan McCaskill <101627963@student.swin.edu.au>
2021-01-31 13:44:57 +1100

    CHANGE: Added smoother camera movement to centre on the unit whose turn it is

--- HoochGang3D/Assets/Scripts/CameraController.cs
+++ HoochGang3D/Assets/Scripts/CameraController.cs
@@ -9,6 +9,9 @@ public class CameraController : MonoBehaviour
     public float minZoom = 10f;
     public float maxZoom = 30f;
     public Camera thisCamera;
+    private Transform placeToCentreOn;
+    private bool centred = true;
+    public int centreSpeed = 9;
 
     // Update is called once per frame
     void Update()
@@ -23,51 +26,72 @@ public class CameraController : MonoBehaviour
             return;
         }*/
 
-        if (Input.GetKey(KeyCode.LeftShift))
+        if(centred) //if it's the start or we've already centred ourselves on a character
         {
-            panSpeed = 65f;
-        }
-        else
-        {
-            panSpeed = 30f;
+            //recieve user input
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                panSpeed = 65f;
+            }
+            else
+            {
+                panSpeed = 30f;
+            }
+
+            if(Input.GetKey("w"))
+            //if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+            {
+                Vector3 isometricUp = new Vector3(1,0,1); //Forward + Right
+                transform.Translate(isometricUp * panSpeed * Time.deltaTime, Space.World);
+            }
+
+            if(Input.GetKey("a"))
+            //if(Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+            {
+                Vector3 isometricLeft = new Vector3(-1,0,1); //Forward + Left
+                transform.Translate(isometricLeft * panSpeed * Time.deltaTime, Space.World);
+            }
+
+            if(Input.GetKey("s"))
+            //if(Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+            {
+                Vector3 isometricDown = new Vector3(-1,0,-1); //Back + Left
+                transform.Translate(isometricDown * panSpeed * Time.deltaTime, Space.World);
+            }
+            
+            if(Input.GetKey("d"))
+            //if(Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+            {
+                Vector3 isometricRight = new Vector3(1,0,-1); //Back + Right
+                transform.Translate(isometricRight * panSpeed * Time.deltaTime, Space.World);
+            }
+            
+            float scroll = Input.GetAxis("Mouse ScrollWheel");
+            thisCamera.orthographicSize -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+            thisCamera.orthographicSize = Mathf.Clamp(thisCamera.orthographicSize, minZoom, maxZoom);    
         }
 
-        if(Input.GetKey("w"))
-        //if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        if(Vector3.Distance(transform.position, placeToCentreOn.position) <= 0.5) //if the distances are within 0.5 units of eachother
         {
-            Vector3 isometricUp = new Vector3(1,0,1); //Forward + Right
-            transform.Translate(isometricUp * panSpeed * Time.deltaTime, Space.World);
-        }
+            centred = true;
+        }    
+    }
 
-        if(Input.GetKey("a"))
-        //if(Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+    private void FixedUpdate()
+    {
+        if(!centred)
         {
-            Vector3 isometricLeft = new Vector3(-1,0,1); //Forward + Left
-            transform.Translate(isometricLeft * panSpeed * Time.deltaTime, Space.World);
+            //move towards the character we want to centre on, the two lines below have a similar behaviour
+            transform.Translate((placeToCentreOn.position - transform.position) * panSpeed / centreSpeed  * Time.deltaTime, Space.World);
+            //transform.position = Vector3.Lerp(placeToCentreOn.position, transform.position, 0.5f);
         }
-
-        if(Input.GetKey("s"))
-        //if(Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
-        {
-            Vector3 isometricDown = new Vector3(-1,0,-1); //Back + Left
-            transform.Translate(isometricDown * panSpeed * Time.deltaTime, Space.World);
-        }
-        
-        if(Input.GetKey("d"))
-        //if(Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
-        {
-            Vector3 isometricRight = new Vector3(1,0,-1); //Back + Right
-            transform.Translate(isometricRight * panSpeed * Time.deltaTime, Space.World);
-        }
-        
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        thisCamera.orthographicSize -= scroll * 1000 * scrollSpeed * Time.deltaTime;
-        thisCamera.orthographicSize = Mathf.Clamp(thisCamera.orthographicSize, minZoom, maxZoom);
     }
 
     public void CentreCameraOnTransform(Transform t)
     {
-        transform.position = t.position; //set the camera transform the position of the character whose turn it is
+        centred = false;
+        placeToCentreOn = t;
+        //transform.position = t.position; //set the camera transform the position of the character whose turn it is
         //the above code is really jarring, and it lets the user know there is a unit that can move, but it is not clear what's happening when it changes.
         //In future, I think it would be better to use a cooroutine, see below
         //start a coroutine that translates the camera position to the transform we want to centre on
