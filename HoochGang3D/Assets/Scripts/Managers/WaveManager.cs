79f6b5f71d00ca3bb85a997e9720fd3d63c79cd0
Lachlan McCaskill <101627963@student.swin.edu.au>
2021-02-09 17:13:33 +1100

    NEW AND CHANGE: Added a wave manager class that can spawn waves and updated the wave serializable

---
+++ HoochGang3D/Assets/Scripts/Managers/WaveManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    private int currentWave = 0;
+
+    public Wave[] waves;
+
+    public void SpawnWave()
+    {
+        //instantiate an enemy at each transform defined in the current wave
+        foreach(Transform t in waves[currentWave].startLocations)
+        {
+            Instantiate(waves[currentWave].enemy, t.position, Quaternion.identity);
+        }
+    }
+
+    public void NextWave()
+    {
+        currentWave++;
+    }
+}
--- HoochGang3D/Assets/Scripts/Wave.cs
+++ HoochGang3D/Assets/Scripts/Wave.cs
@@ -4,11 +4,5 @@
 public class Wave
 {
     public GameObject enemy;
-    public int count;
     public Transform[] startLocations;
-
-    private void Start()
-    {
-        count = startLocations.Length;
-    }
 }
