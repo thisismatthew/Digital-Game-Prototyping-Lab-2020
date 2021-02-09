64eb740ab5600323796209712634f3266ae2f28b
Lachlan McCaskill <101627963@student.swin.edu.au>
2021-02-09 15:34:11 +1100

    NEW: Added a serialisable class to be used for waves (want to follow a similar pattern to tower defense games

---
+++ HoochGang3D/Assets/Scripts/Wave.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Wave
+{
+    public GameObject enemy;
+    public int count;
+    public Transform[] startLocations;
+
+    private void Start()
+    {
+        count = startLocations.Length;
+    }
+}
