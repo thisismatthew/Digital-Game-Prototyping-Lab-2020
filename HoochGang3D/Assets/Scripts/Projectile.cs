81118926141df97b62b0fd9210924ea36d1ffb6f
Lachlan McCaskill <101627963@student.swin.edu.au>
2021-02-22 08:54:05 +1100

    CHANGE: Started work on rotation code for sprites on projectiles

--- HoochGang3D/Assets/Scripts/Projectile.cs
+++ HoochGang3D/Assets/Scripts/Projectile.cs
@@ -8,11 +8,12 @@ public class Projectile : MonoBehaviour
     public void Seek(Transform _target)
     {
         target = _target;
+        //PointAtTarget(target.transform.position);
     }
 
     void Update()
     {
-        transform.rotation = Quaternion.identity;
+        //transform.rotation = Quaternion.identity;
 
         if(target == null)
         {
@@ -46,5 +47,41 @@ public class Projectile : MonoBehaviour
         else
             enemy.gameObject.GetComponentInChildren<Animator>().Play("Goblin_death");
         Destroy(enemy.gameObject, 3f);
+    }
+
+    private void PointAtTarget(Vector3 targetPos)
+    {
+        //Get the transform of the child sprite
+        Quaternion spriteRotation = GetComponentInChildren<Transform>().rotation;
+        Vector3 debugForward = Vector3.forward;
+
+        //find the direction that the target is in
+        Vector3 dir = Vector3.Normalize(transform.position - targetPos);
+
+        if(dir.normalized.Equals(Vector3.forward)) //up + left
+        {
+            //set rotation of projectile in Z axis to 25
+            spriteRotation.eulerAngles = new Vector3(spriteRotation.x, spriteRotation.y, 25);
+        }
+        else if(dir.normalized.Equals(Vector3.back)) //down + right
+        {
+            //set rotation of projectile in Z axis to 208
+            spriteRotation.eulerAngles = new Vector3(spriteRotation.x, spriteRotation.y, 208);
+        }
+        else if(dir.normalized.Equals(Vector3.left)) //down + left
+        {
+            //set rotation of projectile in Z axis to 95
+            spriteRotation.eulerAngles = new Vector3(spriteRotation.x, spriteRotation.y, 95);
+        }
+        else if(dir.normalized.Equals(Vector3.right)) //up + right
+        {
+            //set rotation of projectile in Z axis to 280
+            spriteRotation.eulerAngles = new Vector3(spriteRotation.x, spriteRotation.y, 280);
+        }
+        else
+        {
+            Debug.Log("Projectile is pointing in direction: " + dir.ToString() + ". This is not properly handled.");
+            Debug.Log(Vector3.forward.ToString());
+        }
     }
 }
