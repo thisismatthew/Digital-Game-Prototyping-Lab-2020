ce16b5169408eb18e3e10f3065f3d69f0d82a32c
Matthew <102381833@student.swin.edu.au>
2021-03-04 13:08:13 +1100

    ADD: Transparency Script to nodes around center church CHANGE: the hight of the Invisible obstacle

---
+++ HoochGang3D/Assets/Scripts/MouseOverTransparency.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOverTransparency : MonoBehaviour
+{
+    public GameObject buildingSprite;
+    public Color startColor;
+    public Color mouseOverColor;
+    bool mouseOver = false;
+
+    private void Start()
+    {
+        startColor = buildingSprite.GetComponent<Renderer>().material.GetColor("_Color");
+    }
+
+    private void OnMouseEnter()
+    {
+        mouseOver = true;
+        buildingSprite.GetComponent<Renderer>().material.SetColor("_Color", mouseOverColor);
+    }
+
+    private void OnMouseExit()
+    {
+        mouseOver = false;
+        buildingSprite.GetComponent<Renderer>().material.SetColor("_Color", startColor);
+    }
+}
